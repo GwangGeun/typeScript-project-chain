4ff57e4c1dfa83dc80f6e7218069324472510864
GwangGeun <jgg0328@gmail.com>
2020-03-09 22:32:25 +0900

    class test

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "gwanggeun",
-  age: 28,
-  gender: "male"
-};
+const person = new Human("GwangGeun", 28, "male");
+
+// const person = {
+//   name: "gwanggeun",
+//   age: 28,
+//   gender: "male"
+// };
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
@@ -16,4 +29,4 @@ const sayHi = (person: Human): string => {
 
 console.log(sayHi(person));
 
-export {};
+// export {};
